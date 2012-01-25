8e3774c5868226f0ba91da9d4031070a000a0b8c
Srinivas Singanamalla <ssinganamalla@Srinivass-MacBook-Pro.local>
2012-01-25 08:02:20 -0500

    random changes

--- testgit/testgitTests/testgitTests.m
+++ testgit/testgitTests/testgitTests.m
@@ -26,7 +26,7 @@
 
 - (void)testExample
 {
-    STFail(@"Unit tests are not implemented yet in testgitTests");
+    STFail(@"Unit tests are not implemented yet in testgitTests. Some changes");
 }
 
 @end
