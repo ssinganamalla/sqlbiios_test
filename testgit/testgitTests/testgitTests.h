0e756c7ad9f683d7718714ebd0a252b73ae7442f
Srinivas Singanamalla <ssinganamalla@Srinivass-MacBook-Pro.local>
2012-01-25 07:56:58 -0500

    initial commit

---
+++ testgit/testgitTests/testgitTests.h
@@ -0,0 +1,13 @@
+//
+//  testgitTests.h
+//  testgitTests
+//
+//  Created by Srinivas Singanamalla on 1/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface testgitTests : SenTestCase
+
+@end
---
+++ testgit/testgitTests/testgitTests.m
@@ -0,0 +1,32 @@
+//
+//  testgitTests.m
+//  testgitTests
+//
+//  Created by Srinivas Singanamalla on 1/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "testgitTests.h"
+
+@implementation testgitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in testgitTests");
+}
+
+@end
